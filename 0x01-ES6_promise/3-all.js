d9688d94a55e8ac3c8a30c0c990d1a6d6d19087c
p-prime1 <preciousnwaka95@gmail.com>
2024-10-05 10:38:39 +0100

    a

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import uploadPhoto, { createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+  } catch (error) {
+    console.log(Error, 'error');
+  }
+}
