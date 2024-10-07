ce178c7f4afec40f829b234b7a470e0d004a894b
p-prime1 <preciousnwaka95@gmail.com>
2024-10-07 17:04:42 +0100

    Function returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const newArray = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firtName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return (newArray);
+}
