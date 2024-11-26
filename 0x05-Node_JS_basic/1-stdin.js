a18606c1301b6562da6811c49dd2b3f9de9d49e3
p-prime1 <preciousnwaka95@gmail.com>
2024-11-26 23:25:18 +0100

    used process.exit instead of SIGINT

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,6 @@ process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 });
-process.on('SIGINT', () => {
+process.on('exit', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
